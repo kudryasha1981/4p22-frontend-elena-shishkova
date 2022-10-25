680ced64008a5900f720368d778df29e8ced590d
kudryasha1981 <105546279+kudryasha1981@users.noreply.github.com>
2022-10-25 20:41:08 +0300

    add intermediate_certification

---
+++ lesson_3.2/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const result = 5 + 10 / 4 * 8 - 6;
+console.log(result);  
+
+const division = 15 % 4;
+console.log(division);
+
+const pow = 4**3;
+console.log(pow);
+
+const divisionByZero = 1 / 0;
+console.log(divisionByZero)
+
