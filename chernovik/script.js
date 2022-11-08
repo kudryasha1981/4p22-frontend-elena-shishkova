ccd7ee410912f64b8445823c02d5bdf01b43922d
kudryasha1981 <105546279+kudryasha1981@users.noreply.github.com>
2022-11-08 14:19:21 +0300

    add homework_8

--- chernovik/script.js
+++ chernovik/script.js
@@ -1,29 +1,11 @@
 'use strict';
 
-function calc() {
-    const firstOperand = Number(document.forms['calc'].elements['firstOperand'].value);
-    const secondOperand = Number(document.forms['calc'].elements['secondOperand'].value);
-    const operator = document.forms['calc'].elements['operator'].value;
-    
-    switch (operator) {
-        case '-':
-            alert(`Ответ: ${firstOperand - secondOperand}`);
-            break;
-
-        case '+':
-            alert(`Ответ: ${firstOperand + secondOperand}`);
-            break;
-
-        case '/':
-            alert(`Ответ: ${firstOperand / secondOperand}`);
-            break;
-
-        case '*':
-            alert(`Ответ: ${firstOperand * secondOperand}`);
-            break;
-            
-        default:
-            alert('Не знаю, что вы от меня хотите');
+let fifthLetter = function (name) {
+    if (name.length < 5) {
+        return;
     }
+
+    return 'пятая буква вашего имени:' + name[4] + '.';
 }
 
+console.log(fifthLetter('ник'))
